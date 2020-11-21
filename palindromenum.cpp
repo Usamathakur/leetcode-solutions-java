82fd4f58891910e047c3729e6767d987af967bf2
Usamathakur <51357843+Usamathakur@users.noreply.github.com>
2020-11-21 16:58:11 +0530

    Create palindromenum.cpp

---
+++ palindromenum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || x >= 2147483647)
+            return false;
+        long int num = 0;
+        
+        int y = x;
+        
+        while(x>0){
+            num = num*10;
+            if(num > 2147483647){
+                return false;
+            }
+            num = num+(x%10);
+            x=x/10;
+        }
+        if(num==y){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
